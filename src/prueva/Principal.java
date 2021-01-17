63dd01a5457d46874b2d04c75e1a92417a79ea30
Jonathan <Jony-hackmc@gmail.com>
2021-01-17 13:55:54 -0600

    cambios menores

--- src/prueva/Principal.java
+++ src/prueva/Principal.java
@@ -14,9 +14,10 @@ public class Principal {
         Session session = miFac.openSession();
         session.beginTransaction();
 
-        Producto product = session.get(Producto.class, 1);
-        System.out.println(product.getModelo());
+        Producto product = session.get(Producto.class, 1); 
+        //Producto nuevo=new Producto(2,1,"M4",20,"El huawei hibrido","1589112461","{\"Ram\":\"4GB\"}");
         //session.save(product);
+        //session.save(nuevo);
         session.getTransaction().commit();
     }
 
