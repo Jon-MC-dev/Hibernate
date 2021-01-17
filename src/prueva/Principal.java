186e94284f467e1fc71066695c4922b5c226b90d
Jonathan <Jony-hackmc@gmail.com>
2021-01-17 13:07:19 -0600

    cambios menores

--- src/prueva/Principal.java
+++ src/prueva/Principal.java
@@ -7,26 +7,19 @@ import org.hibernate.cfg.Configuration;
 public class Principal {
 
     public static void main(String[] args) {
-//        SessionFactory miFac = new Configuration().configure("./prueva/hibernate.conf.xml").
-//                addAnnotatedClass(Producto.class).
-//                addAnnotatedClass(Categoria.class).
-//                buildSessionFactory();
-//        Session session = miFac.openSession();
-//        session.beginTransaction();
-//
-//        Producto product = session.get(Producto.class, 1);
-//        System.out.println(product.getModelo());
-//        //session.save(product);
-//        session.getTransaction().commit();
-        numerosPares();
+        SessionFactory miFac = new Configuration().configure("./prueva/hibernate.conf.xml").
+                addAnnotatedClass(Producto.class).
+                addAnnotatedClass(Categoria.class).
+                buildSessionFactory();
+        Session session = miFac.openSession();
+        session.beginTransaction();
+
+        Producto product = session.get(Producto.class, 1);
+        System.out.println(product.getModelo());
+        //session.save(product);
+        session.getTransaction().commit();
     }
 
-    public static void numerosPares() {
-        for (int numero = 1; numero <= 100; numero++) {
-            if (numero % 2 == 0) {
-                System.out.print(numero + ",");
-            }
-        }
-    }
+    
 
 }
